03a2a9e21eb0ef09f402d5c076cce846709c4eb2
pravallikaadineni <pravallikaadineni143@gmail.com>
2022-10-21 11:15:48 +0530

    Add project files.

---
+++ CalciLibJenkisDemo/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalciLibJenkisDemo
+{
+    public class Class1
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
